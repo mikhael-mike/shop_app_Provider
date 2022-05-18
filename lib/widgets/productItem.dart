6ca1c20b5da1323bba921cb68e14ebc854aa4ce4
mikhael_mike <mnosson@gmail.com>
2022-05-18 23:36:08 +0400

    Error Consumer to IconButton
    Iconbutton leading Error. don't show the favorite icon

--- lib/widgets/productItem.dart
+++ lib/widgets/productItem.dart
@@ -14,44 +14,43 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
     print('product rebuild');
     return ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: GridTile(
-          child: GestureDetector(
-              onTap: () {
-                Navigator.pushNamed(context, ProductDetail.routePage,
-                    arguments: product.id);
-              },
-              child: Image.network(
-                product.imageUrl,
-                fit: BoxFit.cover,
-              )),
-          footer: GridTileBar(
-            backgroundColor: Colors.black87,
-            title: Text(
-              product.title,
-              textAlign: TextAlign.center,
-            ),
-            leading: Consumer<Product>(
-              builder: (context, value, _) => IconButton(
-                  onPressed: () {
-                    product.toggleFavoriteStatus();
-                  },
-                  color: Theme.of(context).primaryColor,
-                  icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border)
-                  ),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              color: Theme.of(context).primaryColor,
-              icon: Icon(Icons.shopping_bag),
-            ),
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, ProductDetail.routePage,
+                  arguments: product.id);
+            },
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
+            )),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
           ),
-        
+          leading: Consumer<Product>(
+            builder: (context, value, _) => IconButton(
+                onPressed: () {
+                  product.toggleFavoriteStatus();
+                },
+                color: Theme.of(context).primaryColor,
+                icon: Icon(product.isFavorite
+                    ? Icons.favorite
+                    : Icons.favorite_border)),
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            color: Theme.of(context).primaryColor,
+            icon: Icon(Icons.shopping_bag),
+          ),
         ),
-
+      ),
     );
   }
 }
