339f99262c828ca2821a15c6fb1dd07f1d8688c3
mikhael_mike <mnosson@gmail.com>
2022-05-18 22:41:13 +0400

    Use Consumer
    use consumer in productitem
    builder(context, value, child) => ..
    
    value is a value of data

--- lib/widgets/productItem.dart
+++ lib/widgets/productItem.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shop_app/providers/product.dart';
 import 'package:shop_app/screens/product_detail.dart';
 import 'package:provider/provider.dart';
+import 'package:provider/provider.dart';
 
 class ProductItem extends StatelessWidget {
   // final String id;
@@ -13,38 +14,43 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        child: GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(context, ProductDetail.routePage,
-                  arguments: product.id);
-            },
-            child: Image.network(
-              product.imageUrl,
-              fit: BoxFit.cover,
-            )),
-        footer: GridTileBar(
-          backgroundColor: Colors.black87,
-          title: Text(
-            product.title,
-            textAlign: TextAlign.center,
-          ),
-          leading: IconButton(
-              onPressed: () {
-                product.toggleFavoriteStatus();
+    // final product = Provider.of<Product>(context) ;
+    return Consumer<Product>(
+      builder: ((context, value, child) => ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+          child: GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, ProductDetail.routePage,
+                    arguments: value.id);
               },
+              child: Image.network(
+                value.imageUrl,
+                fit: BoxFit.cover,
+              )),
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            title: Text(
+              value.title,
+              textAlign: TextAlign.center,
+            ),
+            leading: IconButton(
+                onPressed: () {
+                  value.toggleFavoriteStatus();
+                },
+                color: Theme.of(context).primaryColor,
+                icon: Icon(value.isFavorite ? Icons.favorite : Icons.favorite_border)),
+            trailing: IconButton(
+              onPressed: () {},
               color: Theme.of(context).primaryColor,
-              icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border)),
-          trailing: IconButton(
-            onPressed: () {},
-            color: Theme.of(context).primaryColor,
-            icon: Icon(Icons.shopping_bag),
+              icon: Icon(Icons.shopping_bag),
+            ),
           ),
         ),
-      ),
+
+      ))
+    
+      
     );
   }
 }
