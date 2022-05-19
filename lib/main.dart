db704d43af6d1f99d9b33ea5b2d42a10e786fad5
mikhael_mike <mnosson@gmail.com>
2022-05-19 23:47:03 +0400

    Multiple Provider
    Configutation Multiple provider in main.dart for Products() and Cart()

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/providers/cart.dart';
 import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/screens/product_detail.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
-
 
 void main() => runApp(MyApp());
 
@@ -12,15 +12,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: ((context) =>  Products()),
+        ),
+        ChangeNotifierProvider(
+          create: ((context) =>  Cart()),
+        ),
+        // create: (context) => Products(),
+      ],
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
           primarySwatch: Colors.purple,
           primaryColor: Colors.amber,
           fontFamily: 'Lato',
-          
         ),
         routes: {
           ProductDetail.routePage: ((context) => ProductDetail()),
@@ -30,5 +37,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
